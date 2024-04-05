25d284aa03ca4c1953e3ba4b4df19d0d6da10070
Vivek Tondare <53465016+Vicky-7875@users.noreply.github.com>
2024-04-05 15:41:19 +0000

    converstuion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -19,7 +19,47 @@
 //"vivek"=> true;
 ///1 =>true ; 0=>falsee
 
-let someNumber =33;
-let stringNumber = String(someNumber);
-console.log(stringNumber)
-console.log(typeof stringNumber) //string
+// let someNumber =33;
+// let stringNumber = String(someNumber);
+// console.log(stringNumber)
+// console.log(typeof stringNumber) //string
+
+
+/******   Operations************************* */
+
+let value =3;
+let negValue= -value;
+// console.log(negValue)
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+
+let str1 ='vivek';
+let str2 = ' hello'
+
+let stri3 =str1+str2;
+// console.log(stri3)
+
+// console.log("1"+2); //12
+// console.log(1+"2"); //12
+// console.log("1"+"2"); //12
+// console.log("1"+2 +2); //122
+// console.log(1+2 +"2"); //32
+
+
+// console.log(+true) //1
+// console.log(+"") //0
+
+let num1, num2 , num3;
+num1 = num2 =num3 =2+2;
+
+let gamecounter =100;
+// gamecounter++;  //101
+++gamecounter;  //101
+console.log(gamecounter)

bac710baeb074e4194ac9d0d49e90391b070ba69
Vivek Tondare <53465016+Vicky-7875@users.noreply.github.com>
2024-04-05 15:01:43 +0000

    variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=14444;
+let accountEmail ="vivek@gmail.com";
+var accountPass="12345";
+accountCity="Nagpur";
+let accountState;
+// accountId=2;  //not allowed
+accountEmail="vickytibadre@gmial.com";
+accountPass='222211';
+accountCity='npappa'
+// console.log(accountId)
+/**
+ * prefare not to use var
+ * because it has scope problem
+ */
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])

8285b1e2f9ce8f3d576a1ea2547d695558458b83
Vivek Tondare <53465016+Vicky-7875@users.noreply.github.com>
2024-04-05 15:27:41 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+// let score =undefined;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber=Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber)
+
+//"33"=> 33
+//"33abc" =>NaN
+//true =>1; false =>0
+
+// let isLoggedIn ='jj';
+// let booleanIsLogged=Boolean(isLoggedIn);
+// console.log(booleanIsLogged)
+
+//"" => false;
+//"vivek"=> true;
+///1 =>true ; 0=>falsee
+
+let someNumber =33;
+let stringNumber = String(someNumber);
+console.log(stringNumber)
+console.log(typeof stringNumber) //string

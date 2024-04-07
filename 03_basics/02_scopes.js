29e215ead0468f4a767b81457cccd0ce99604035
Vivek Tondare <53465016+Vicky-7875@users.noreply.github.com>
2024-04-07 11:05:41 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,17 @@
+// {
+// } //scope
+let a = 300;
+
+if (true) {
+  let a = 10;
+  const b = 20;
+  console.log("inner",a);
+}
+
+console.log(a);
+// console.log(b);
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
